1f2f71c9d9f95865c79f4115e0b6c656d2b60847
rennyboy <rennyboyjr@gmail.com>
2026-04-29 15:27:12 +0800

    updated and fix the login that is broken

--- app/Filament/Resources/EquipmentResource/Pages/CreateEquipment.php
+++ app/Filament/Resources/EquipmentResource/Pages/CreateEquipment.php
@@ -1,7 +1,19 @@
 <?php
+
 namespace App\Filament\Resources\EquipmentResource\Pages;
+
 use App\Filament\Resources\EquipmentResource;
 use Filament\Resources\Pages\CreateRecord;
-class CreateEquipment extends CreateRecord {
+use Illuminate\Support\Facades\Auth;
+
+class CreateEquipment extends CreateRecord
+{
     protected static string $resource = EquipmentResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['school_id'] = Auth::user()->school_id;
+
+        return $data;
+    }
 }
