eacb82aecf2c00ea5ccf2279a948af00ae41c20e
rennyboy <rennyboyjr@gmail.com>
2026-04-30 17:16:49 +0800

    added resetpassword resource on the superadmin side to reset for users

---
+++ app/Filament/Resources/PasswordResetResource/Pages/ListPasswordResets.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\PasswordResetResource\Pages;
+
+use App\Filament\Resources\PasswordResetResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPasswordResets extends ListRecords
+{
+    protected static string $resource = PasswordResetResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+}
