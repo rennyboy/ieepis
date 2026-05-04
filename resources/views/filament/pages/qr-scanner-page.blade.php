1e1727444a0180838b8031d8ed5467ec3ef046a5
rennyboy <rennyboyjr@gmail.com>
2026-05-04 10:39:46 +0800

    refactor: import html5-qrcode as a module and remove external CDN dependency

--- resources/views/filament/pages/qr-scanner-page.blade.php
+++ resources/views/filament/pages/qr-scanner-page.blade.php
@@ -4,8 +4,5 @@
     @push('scripts')
         <!-- Vue Scanner Entry Point -->
         @vite(['resources/js/scanner.js'])
-        
-        <!-- html5-qrcode CDN -->
-        <script src="https://unpkg.com/html5-qrcode@2.3.8/minified/html5-qrcode.min.js"></script>
     @endpush
 </x-filament::page>
